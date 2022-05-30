58bfac6d1f6615d5abac2242112bbf252552ec8a
Hello! Human <jeffernmalinao82@gmail.com>
2022-05-30 13:23:39 +0800

    New update

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,8 @@
 const body = (document.querySelector("body").style.background = "#282828");
 $(document).ready(function () {
-  $("body").css("color", "green");
+  $("body").css("color", "#282828");
 });
+
+$(document).on('click', '.header .navbar .navbar-nav .nav-item', function(){
+  $(this).addClass('function').siblings().removeClass('function');
+})
--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,2 @@
-"use strict";var body=document.querySelector("body").style.background="#282828";$(document).ready((function(){$("body").css("color","green")}));
+"use strict";var body=document.querySelector("body").style.background="#282828";$(document).ready((function(){$("body").css("color","#282828")})),$(document).on("click",".header .navbar .navbar-nav .nav-item",(function(){$(this).addClass("function").siblings().removeClass("function")}));
 //# sourceMappingURL=app.js.map
